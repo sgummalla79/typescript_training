2e9f342a95fd7a5f0fdc0134cde9dac650f9092f
Suman <sgummalla.work@gmail.com>
2017-09-10 13:31:48 -0700

    js folder deleted

--- scripts/js/lib/utiltityFunctions.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function CalculateLateFee(daysLate) {
-    return daysLate * 0.25;
-}
-exports.CalculateLateFee = CalculateLateFee;
-//# sourceMappingURL=utiltityFunctions.js.map
